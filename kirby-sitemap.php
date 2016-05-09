b6e63264f5006a02433298da500618119923692c
Pascal Küsgen <pascal@pascalmh.de>
2016-05-09 11:03:43 +0200

    feat: add indicate alternate language
    using "prefixing the prefix"-hack found at - http://stackoverflow.com/a/6928183

--- kirby-sitemap.php
+++ kirby-sitemap.php
@@ -37,12 +37,23 @@ kirby()->routes(array(
 
             $sitemap = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset />');
             $sitemap->addAttribute('xmlns', "http://www.sitemaps.org/schemas/sitemap/0.9");
+            $sitemap->addAttribute('xmlns:xmlns:xhtml', "http://www.w3.org/1999/xhtml"); // using "prefixing the prefix"-hack found at - http://stackoverflow.com/a/6928183
 
             foreach (site()->pages()->index() as $p) {
                 if (!sitemapRouteIsExcluded($p->uri())) {
                     $url = $sitemap->addChild("url");
                     $url->addChild("loc", html($p->url()));
                     $url->addChild("lastmod", $p->modified('c'));
+
+                    $languages = site()->languages();
+                    if ($languages && $languages->count() > 1) {
+                        foreach($languages as $language) {
+                            $link = $url->addChild('xhtml:xhtml:link'); // using "prefixing the prefix"-hack found at - http://stackoverflow.com/a/6928183
+                            $link->addAttribute("rel", "alternate");
+                            $link->addAttribute("hreflang", html($language->code()));
+                            $link->addAttribute("href", html($p->url($language->code())));
+                        }
+                    }
 
                     if(c::get('sitemap.priority', [])) {
                         $url->addChild("priority", ($p->isHomePage() || sitemapRouteIsImportant($p->uri())) ? 1 : 0.6/$p->depth());
